590e1259d5de98aca1458990606925dd40692a72
ricarellano <rarellanob@gmail.com>
2020-08-23 22:27:52 -0700

    add filtered to contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,34 @@
 import React, { Fragment, useContext } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext);
+  const contactContext = useContext(ContactContext);
 
-    const { contacts } = contactContext;
-    return (
-        <Fragment>
-            {contacts.map(contact => (
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
                 <ContactItem key={contact.id} contact={contact} />
+              </CSSTransition>
             ))}
-        </Fragment>
-    )
-}
+      </TransitionGroup>
+    </Fragment>
+  );
+};
  
 export default Contacts;
 
